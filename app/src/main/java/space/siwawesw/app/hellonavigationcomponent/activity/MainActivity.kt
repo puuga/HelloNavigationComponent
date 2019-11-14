ef5610d1c0f1411845a0b7790f8d010d10011d04
Siwawes Wongcharoen <siwaoh@gmail.com>
2019-11-14 23:58:06 +0700

    1st

---
+++ app/src/main/java/space/siwawesw/app/hellonavigationcomponent/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package space.siwawesw.app.hellonavigationcomponent.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import space.siwawesw.app.hellonavigationcomponent.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
